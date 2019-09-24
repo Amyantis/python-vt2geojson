6f668373eb45b46d95db8a7178cb4e2251330ea3
Théophile Dancoisne <dancoisne.theophile@gmail.com>
2019-09-24 17:12:54 +0200

    Fix get_distribution.

--- vt2geojson/__init__.py
+++ vt2geojson/__init__.py
@@ -1,4 +1,4 @@
-from pkg_resources import get_distribution
+from pkg_resources import get_distribution, DistributionNotFound
 
 NAME = "vt2geojson"
 DESCRIPTION = "Dump vector tiles to GeoJSON from remote URLs or local system files."
@@ -6,4 +6,8 @@ AUTHOR = "Theophile Dancoisne"
 AUTHOR_EMAIL = "dancoisne.theophile@gmail.com"
 URL = "https://github.com/Amyantis/python-vt2geojson"
 
-__version__ = get_distribution(__name__).version
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
